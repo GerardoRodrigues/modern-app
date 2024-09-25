349a5d5fc88b8aa17c8651519df1ac11375dde39
Gerardo Rodrigues <gerardofilho56@gmail.com>
2024-09-25 11:16:18 -0300

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,16 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+import * as dotenv from 'dotenv'
+dotenv.config();
+
+interface ProcessEnv {
+  API_KEY?: string;
+}
+
 export const environment = {
-  production: false
+  production: false,
+  apiKey: (process.env as ProcessEnv).API_KEY
 };
 
 /*
